54c8d84e228a36270da6fe1e51943e7d9ded722f
lucas <lucas@localhost.localdomain>
2017-02-06 21:21:09 -0300

    columnId

--- db/migrate/20170202150429_add_column_to_rubro.rb
+++ db/migrate/20170202150429_add_column_to_rubro.rb
@@ -1,6 +1,5 @@
 class AddColumnToRubro < ActiveRecord::Migration
   def change
-  	add_column :rubros, :id, :int
   	add_column :rubros, :nombre, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202145851) do
+ActiveRecord::Schema.define(version: 20170202150429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20170202145851) do
   create_table "rubros", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "nombre"
   end
 
   create_table "users", force: :cascade do |t|
